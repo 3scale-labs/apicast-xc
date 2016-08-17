9fce8da8d19258cb9fcd3e68118347cde58d2883
David Ortiz <z.david.ortiz@gmail.com>
2016-08-17 15:37:11 +0200

    spec/cache: test handling of Redis errors

--- spec/cache_spec.lua
+++ spec/cache_spec.lua
@@ -1,6 +1,7 @@
 describe('cache', function()
   local cache
   local redis_client
+  local redis_pool
 
   setup(function()
     -- Mock the redis pool. For testing, we'll use redis-lua instead of resty.redis
@@ -25,6 +26,7 @@ describe('cache', function()
       release = function() return true end
     }
 
+    redis_pool = package.loaded.redis_pool
     cache = require 'cache'
   end)
 
@@ -73,12 +75,45 @@ describe('cache', function()
         assert.is_nil(cached_auth)
       end)
     end)
+
+    describe('when there is an error acquiring a Redis connection', function()
+      setup(function()
+        redis_pool.acquire = function() return nil, false end
+      end)
+
+      it('returns an error', function()
+        local _, ok = cache.authorize(service_id, app_id, method)
+        assert.is_false(ok)
+      end)
+
+      teardown(function()
+        redis_pool.acquire = function() return redis_client, true end
+      end)
+    end)
+
+    describe('when there is an error checking the auth in Redis', function()
+      setup(function()
+        redis_pool.acquire = function()
+          return { hget = function() return nil, true end }, true
+        end
+      end)
+
+      it('returns an error', function()
+        local _, ok = cache.authorize(service_id, app_id, method)
+        assert.is_false(ok)
+      end)
+
+      teardown(function()
+        redis_pool.acquire = function() return redis_client, true end
+      end)
+    end)
   end)
 
   describe('report', function()
     local service_id = 'a_service_id'
     local app_id = 'an_app_id'
     local method = 'a_method'
+    local usage_val = 10
 
     -- TODO: Try not to hardcode these 2 here
     local report_key = 'report:'..service_id..':'..app_id
@@ -89,11 +124,10 @@ describe('cache', function()
       redis_client:del(report_keys_set)
     end)
 
-    describe('when the usage is cached', function()
+    describe('when the usage is cached and there are no DB connection errors', function()
       local current_usage = 5
-      local usage_val = 10
 
-      setup(function()
+      before_each(function()
         redis_client:hset(report_key, method, current_usage)
       end)
 
@@ -103,11 +137,13 @@ describe('cache', function()
         local cached_val = redis_client:hget(report_key, method)
         assert.are_equals(current_usage + usage_val, tonumber(cached_val))
       end)
+
+      it('returns true', function()
+        assert.is_true(cache.report(service_id, app_id, method, usage_val))
+      end)
     end)
 
-    describe('when the usage is not cached', function()
-      local usage_val = 10
-
+    describe('when the usage is not cached and there are no DB connection errors', function()
       it('caches the reported value', function()
         cache.report(service_id, app_id, method, usage_val)
 
@@ -118,6 +154,45 @@ describe('cache', function()
       it('adds the report hash key to the set of modified keys', function()
         cache.report(service_id, app_id, method, usage_val)
         assert.are_equals(report_key, redis_client:smembers(report_keys_set)[1])
+      end)
+
+      it('returns true', function()
+        assert.is_true(cache.report(service_id, app_id, method, usage_val))
+      end)
+    end)
+
+    describe('when there is an error acquiring a Redis connection', function()
+      setup(function()
+        redis_pool.acquire = function() return nil, false end
+      end)
+
+      it('returns false', function()
+        assert.is_false(cache.report(service_id, app_id, method, usage_val))
+      end)
+
+      teardown(function()
+        redis_pool.acquire = function() return redis_client, true end
+      end)
+    end)
+
+    describe('when there is an error reporting to Redis', function()
+      setup(function()
+        -- we need to create a Redis client that accepts all the commands used
+        -- in the code and that returns an error in some step
+        redis_pool.acquire = function()
+          return { init_pipeline = function() end,
+                   hincrby = function() end,
+                   sadd = function() end,
+                   commit_pipeline = function() return nil, true end }, true
+        end
+      end)
+
+      it('returns false', function()
+        assert.is_false(cache.report(service_id, app_id, method, usage_val))
+      end)
+
+      teardown(function()
+        redis_pool.acquire = function() return redis_client, true end
       end)
     end)
   end)
